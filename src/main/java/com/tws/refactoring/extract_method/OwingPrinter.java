09fc2683cdd8c824e71eafe63dbfc7e2389f286a
huazui <2509155771@qq.com>
2019-07-15 18:29:04 +0800

    Add files via upload

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -6,24 +6,31 @@ import java.util.List;
 import java.util.Vector;
 
 public class OwingPrinter {
-    void printOwing(String name, List<Order> orders) {
-        Iterator<Order> elements = orders.iterator();
-        double outstanding = 0.0;
+    public void printBanner() {
+        String printFirstAndEnd="*****************************";
+        String printMediem="****** Customer totals ******\n";
+        System.out.println(printFirstAndEnd+"\n"+printMediem+"\n"+printFirstAndEnd);
+    }
+    public void printDetail(String name,double outstanding) {
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
-
-        // print owings
+        System.out.println("name: "+name);
+        System.out.println("amount: "+outstanding);
+    }
+    public double getOwing(Iterator<Order> elements,double outstanding){
         while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
+        return outstanding;
+    }
 
-        // print details
-        System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
+    void printOwing(String name, List<Order> orders) {
+        Iterator<Order> elements = orders.iterator();
+        double outstanding = 0.0;
+         printBanner();
+         outstanding=getOwing(elements,outstanding);
+         printDetail( name,outstanding);
+
     }
 }
 
@@ -37,4 +44,5 @@ class Order {
     public double getAmount() {
         return amount;
     }
+
 }
